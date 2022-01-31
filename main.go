8817f8658712cd63d1d1beca0a14f703c6fbd122
Memo Chou <memochou1993@gmail.com>
2022-01-31 22:03:40 +0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"bytes"
+	"encoding/xml"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/memochou1993/mini-aat/model"
+	"gopkg.in/yaml.v3"
+)
+
+const (
+	RootTitle = "Top of the AAT hierarchies"
+)
+
+var (
+	filename string
+)
+
+func main() {
+	flag.StringVar(&filename, "f", "example.xml", "source file")
+	flag.Parse()
+	if err := convert(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func convert() error {
+	input, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer closeFile(input)
+	decoder := xml.NewDecoder(input)
+	dst := fmt.Sprintf("%s.yaml", strings.TrimSuffix(filename, filepath.Ext(filename)))
+	if err := os.Remove(dst); err != nil {
+		if !os.IsNotExist(err) {
+			return err
+		}
+	}
+	output, err := os.OpenFile(dst, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer closeFile(output)
+	title := "---\nsubjects:\n  "
+	if _, err := output.Write([]byte(title)); err != nil {
+		return err
+	}
+	for {
+		token, err := decoder.Token()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		if token == nil {
+			break
+		}
+		switch element := token.(type) {
+		case xml.StartElement:
+			switch element.Name.Local {
+			case "Subject":
+				subject := model.Subject{}
+				if err := decoder.DecodeElement(&subject, &element); err != nil {
+					return err
+				}
+				for i, parent := range subject.ParentRelationships.PreferredParent {
+					s := parent.ParentString
+					if strings.Contains(s, "\n") {
+						s = RootTitle
+					}
+					protectedTerm := findProtectedTerm(s)
+					if protectedTerm != "" {
+						s = strings.Replace(s, protectedTerm, "_", 1)
+					}
+					if i := strings.IndexAny(s, "["); i > -1 {
+						s = s[:i]
+					}
+					if i := strings.IndexAny(s, "("); i > -1 {
+						s = s[:i]
+					}
+					if protectedTerm != "" {
+						s = strings.Replace(s, "_", protectedTerm, 1)
+					}
+					subject.ParentRelationships.PreferredParent[i].ParentString = strings.TrimSuffix(s, " ")
+				}
+				for i, note := range subject.DescriptiveNotes.DescriptiveNote {
+					s := note.NoteText
+					s = strings.ReplaceAll(s, "\n", " ")
+					subject.DescriptiveNotes.DescriptiveNote[i].NoteText = s
+				}
+				var b bytes.Buffer
+				encoder := yaml.NewEncoder(&b)
+				encoder.SetIndent(2)
+				if err := encoder.Encode(&[]model.Subject{subject}); err != nil {
+					return err
+				}
+				s := fmt.Sprintf("%s", strings.ReplaceAll(b.String(), "\n", "\n  "))
+				if _, err := output.Write([]byte(s)); err != nil {
+					return err
+				}
+			}
+		}
+	}
+	return nil
+}
+
+func closeFile(f *os.File) {
+	if err := f.Close(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func findProtectedTerm(s string) string {
+	for _, protectedTerm := range model.ProtectedTerms {
+		if strings.Contains(s, protectedTerm) {
+			return protectedTerm
+		}
+	}
+	return ""
+}
---
+++ model/model.go
@@ -0,0 +1,101 @@
+package model
+
+type Subjects struct {
+	Subjects []Subject `xml:"Subject" bson:"subjects" yaml:"subjects"`
+}
+
+type Subject struct {
+	SubjectID           string `xml:"Subject_ID,attr" bson:"subjectId" yaml:"subjectId"`
+	ParentRelationships struct {
+		PreferredParent    []ParentRelationship `xml:"Preferred_Parent" bson:"preferredParents" yaml:"preferredParents"`
+		NonPreferredParent []ParentRelationship `xml:"Non-Preferred_Parent" bson:"nonPreferredParents" yaml:"nonPreferredParents"`
+	} `xml:"Parent_Relationships" bson:"parentRelationship" yaml:"parentRelationship"`
+	DescriptiveNotes struct {
+		DescriptiveNote []struct {
+			NoteText         string `xml:"Note_Text" bson:"noteText" yaml:"noteText"`
+			NoteLanguage     string `xml:"Note_Language" bson:"noteLanguage" yaml:"noteLanguage"`
+			NoteContributors struct {
+				NoteContributor []struct {
+					ContributorID string `xml:"Contributor_id" bson:"contributorId" yaml:"contributorId"`
+				} `xml:"Note_Contributor" bson:"noteContributors" yaml:"noteContributors"`
+			} `xml:"Note_Contributors" bson:"noteContributor" yaml:"noteContributor"`
+			NoteSources struct {
+				NoteSource []struct {
+					Source Source `xml:"Source" bson:"source" yaml:"source"`
+				} `xml:"Note_Source" bson:"noteSources" yaml:"noteSources"`
+			} `xml:"Note_Sources" bson:"noteSource" yaml:"noteSource"`
+		} `xml:"Descriptive_Note" bson:"descriptiveNotes" yaml:"descriptiveNotes"`
+	} `xml:"Descriptive_Notes" bson:"descriptiveNote" yaml:"note"` // use "note" for thesaurus builder
+	RecordType   string `xml:"Record_Type" bson:"recordType" yaml:"recordType"`
+	MergedStatus string `xml:"Merged_Status" bson:"mergedStatus" yaml:"mergedStatus"`
+	Hierarchy    string `xml:"Hierarchy" bson:"hierarchy" yaml:"hierarchy"`
+	SortOrder    string `xml:"Sort_Order" bson:"sortOrder" yaml:"sortOrder"`
+	Terms        struct {
+		PreferredTerm    []Term `xml:"Preferred_Term" bson:"preferredTerms" yaml:"preferredTerms"`
+		NonPreferredTerm []Term `xml:"Non-Preferred_Term" bson:"nonPreferredTerms" yaml:"nonPreferredTerms"`
+	} `xml:"Terms" bson:"term" yaml:"term"`
+	AssociativeRelationships struct {
+		AssociativeRelationship []AssociativeRelationship `xml:"Associative_Relationship" bson:"associativeRelationships" yaml:"associativeRelationships"`
+	} `xml:"Associative_Relationships" bson:"associativeRelationship" yaml:"associativeRelationship"`
+	SubjectContributors struct {
+		SubjectContributor []struct {
+			ContributorID string `xml:"Contributor_id" bson:"contributorId" yaml:"contributorId"`
+		} `xml:"Subject_Contributor" bson:"subjectContributors" yaml:"subjectContributors"`
+	} `xml:"Subject_Contributors" bson:"subjectContributor" yaml:"subjectContributor"`
+	SubjectSources struct {
+		SubjectSource []struct {
+			Source Source `xml:"Source" bson:"source" yaml:"source"`
+		} `xml:"Subject_Source" bson:"subjectSources" yaml:"subjectSources"`
+	} `xml:"Subject_Sources" bson:"subjectSource" yaml:"subjectSource"`
+}
+
+type ParentRelationship struct {
+	ParentSubjectID  string `xml:"Parent_Subject_ID" bson:"parentSubjectId" yaml:"parentSubjectId"`
+	RelationshipType string `xml:"Relationship_Type" bson:"relationshipType" yaml:"relationshipType"`
+	HistoricFlag     string `xml:"Historic_Flag" bson:"historicFlag" yaml:"historicFlag"`
+	ParentString     string `xml:"Parent_String" bson:"parentString" yaml:"termText"` // use "termText" for thesaurus builder
+	HierRelType      string `xml:"Hier_Rel_Type" bson:"hierRelType" yaml:"hierRelType"`
+}
+
+type AssociativeRelationship struct {
+	RelationshipType string `xml:"Relationship_Type" bson:"relationshipType" yaml:"relationshipType"`
+	RelatedSubjectID struct {
+		VPSubjectID string `xml:"VP_Subject_ID" bson:"vpSubjectId" yaml:"vpSubjectId"`
+	} `xml:"Related_Subject_ID" bson:"relatedSubjectId" yaml:"relatedSubjectId"`
+	HistoricFlag string `xml:"Historic_Flag" bson:"historicFlag" yaml:"historicFlag"`
+}
+
+type Term struct {
+	TermText      string `xml:"Term_Text" bson:"termText" yaml:"termText"`
+	DisplayName   string `xml:"Display_Name" bson:"displayName" yaml:"displayName"`
+	HistoricFlag  string `xml:"Historic_Flag" bson:"historicFlag" yaml:"historicFlag"`
+	Vernacular    string `xml:"Vernacular" bson:"vernacular" yaml:"vernacular"`
+	TermID        string `xml:"Term_ID" bson:"termId" yaml:"termId"`
+	TermLanguages struct {
+		TermLanguage []struct {
+			Language     string `xml:"Language" bson:"language" yaml:"language"`
+			Preferred    string `xml:"Preferred" bson:"preferred" yaml:"preferred"`
+			Qualifier    string `xml:"Qualifier" bson:"qualifier" yaml:"qualifier"`
+			TermType     string `xml:"Term_Type" bson:"termType" yaml:"termType"`
+			PartOfSpeech string `xml:"Part_of_Speech" bson:"partOfSpeech" yaml:"partOfSpeech"`
+			LangStat     string `xml:"Lang_Stat" bson:"langStat" yaml:"langStat"`
+		} `xml:"Term_Language" bson:"termLanguages" yaml:"termLanguages"`
+	} `xml:"Term_Languages" bson:"termLanguage" yaml:"termLanguage"`
+	TermContributors struct {
+		TermContributor []struct {
+			ContributorID string `xml:"Contributor_id" bson:"contributorId" yaml:"contributorId"`
+			Preferred     string `xml:"Preferred" bson:"preferred" yaml:"preferred"`
+		} `xml:"Term_Contributor" bson:"termContributors" yaml:"termContributors"`
+	} `xml:"Term_Contributors" bson:"termContributor" yaml:"termContributor"`
+	TermSources struct {
+		TermSource []struct {
+			Source    Source `xml:"Source" bson:"source" yaml:"source"`
+			Page      string `xml:"Page" bson:"page" yaml:"page"`
+			Preferred string `xml:"Preferred" bson:"preferred" yaml:"preferred"`
+		} `xml:"Term_Source" bson:"termSources" yaml:"termSources"`
+	} `xml:"Term_Sources" bson:"termSource" yaml:"termSource"`
+}
+
+type Source struct {
+	SourceID string `xml:"Source_ID" bson:"sourceId" yaml:"sourceId"`
+}
